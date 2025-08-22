f21bf8e61d71815a6a962b69fddabe5fb4f7bc08
Jeppe Reinhold <jeppe@reinhold.is>
2025-08-22 10:26:19 +0200

    initial VIte plugin with handler

--- src/preset.ts
+++ src/preset.ts
@@ -1,14 +1,22 @@
+import type { Plugin } from "vite";
 // You can use presets to augment the Storybook configuration
 // You rarely want to do this in addons,
 // so often you want to delete this file and remove the reference to it in package.json#exports and package.json#bunder.nodeEntries
 // Read more about presets at https://storybook.js.org/docs/addons/writing-presets
 
 export const viteFinal = async (config: any) => {
-  console.log("This addon is augmenting the Vite config");
-  return config;
-};
-
-export const webpack = async (config: any) => {
-  console.log("This addon is augmenting the Webpack config");
-  return config;
+  const mcpHandlerPlugin: Plugin = {
+    name: "storybook:mcp-server",
+    configureServer(server) {
+      server.middlewares.use("/mcp", (req, res, next) => {
+        console.log("MCP REQUEST!");
+        // Handle requests to the MCP server
+        next();
+      });
+    },
+  };
+  return {
+    ...config,
+    plugins: config.plugins.concat(mcpHandlerPlugin),
+  };
 };
