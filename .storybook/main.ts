0e51c588ca17ff233493da1cdcbe18128bbbcf4a
Jeppe Reinhold <jeppe@reinhold.is>
2025-08-25 22:43:44 +0200

    add complex story structure

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,15 @@
 import { defineMain } from "@storybook/react-vite/node";
 
 const config = defineMain({
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/stories/components/**/*.stories.@(js|jsx|ts|tsx)",
+    {
+      titlePrefix: "Other UI",
+      directory: "../src/stories/other",
+      files: "**/*.stories.@(js|jsx|ts|tsx)",
+    },
+  ],
   addons: ["@storybook/addon-docs", import.meta.resolve("../dist/preset.js")],
   framework: "@storybook/react-vite",
   logLevel: "debug",
