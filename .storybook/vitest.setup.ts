1146c4866d6f8ec3a9c760ac8c01e0d055800403
Jeppe Reinhold <jeppe@reinhold.is>
2025-09-09 08:17:43 -0500

    fix missing annotations

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,8 +1,7 @@
 import { setProjectAnnotations } from "@storybook/react-vite";
 import * as projectAnnotations from "./preview";
-import * as mcpAnnotations from "../src/preview";
 import * as vitestAnnotations from "@storybook/addon-vitest/preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([projectAnnotations, mcpAnnotations, vitestAnnotations]);
+setProjectAnnotations([projectAnnotations, vitestAnnotations]);
