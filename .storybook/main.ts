e0d3cae8111d696a8e08d8cd1890efca7800d46a
Jeppe Reinhold <jeppe@reinhold.is>
2025-10-22 10:55:50 +0200

    move storybook from addon-mcp to root, fix turbo scripting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,15 @@ import { defineMain } from "@storybook/react-vite/node";
 
 const config = defineMain({
   stories: [
-    "../src/**/*.mdx",
-    "../src/stories/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.mdx",
+    "../stories/components/**/*.stories.@(js|jsx|ts|tsx)",
     {
       titlePrefix: "Other UI",
       directory: "../src/stories/other",
       files: "**/*.stories.@(js|jsx|ts|tsx)",
     },
   ],
-  addons: ["@storybook/addon-docs", import.meta.resolve("../dist/preset.js")],
+  addons: ["@storybook/addon-docs", "@storybook/addon-mcp"],
   framework: "@storybook/react-vite",
   logLevel: "debug",
   core: {
