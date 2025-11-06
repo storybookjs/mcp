39426bf4609df7351de21f22332fabf285e7ae47
Jeppe Reinhold <jeppe@reinhold.is>
2025-11-06 17:02:57 +0100

    take screenshots of failed stories too

--- eval/templates/evaluation/.storybook/vitest.setup.ts
+++ eval/templates/evaluation/.storybook/vitest.setup.ts
@@ -2,6 +2,7 @@ import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
 import { setProjectAnnotations } from '@storybook/react-vite';
 import * as projectAnnotations from './preview';
 import { page, server } from 'vitest/browser';
+import { afterEach } from 'vitest';
 
 declare module 'vitest/browser' {
 	interface BrowserCommands {
@@ -9,19 +10,39 @@ declare module 'vitest/browser' {
 	}
 }
 
-// This is an important step to apply the right configuration when testing your stories.
-// More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
+/**
+ * We can only get the story id and canvas element in the story annotations,
+ * but Storybook's afterEach does not run on failed tests.
+ * Therefore we use Storybook's beforeEach hook to store the information,
+ * and then Vitest's afterEach to perform the screenshot and HTML saving,
+ * because that always run.
+ */
+let currentStoryId: string | undefined;
+let currentCanvasElement: Element | undefined;
+
 setProjectAnnotations([
 	a11yAddonAnnotations,
 	projectAnnotations,
 	{
-		afterEach: async ({ id, canvasElement }) => {
-			const resultsPath = await server.commands.getResultsPath();
-			await page.screenshot({ path: `${resultsPath}/${id}.png` });
-			await server.commands.writeFile(
-				`${resultsPath}/${id}.html`,
-				canvasElement.innerHTML,
-			);
+		beforeEach: async ({ id, canvasElement }) => {
+			currentStoryId = id;
+			currentCanvasElement = canvasElement;
+			return () => {
+				currentStoryId = undefined;
+				currentCanvasElement = undefined;
+			};
 		},
 	},
 ]);
+
+afterEach(async () => {
+	if (!currentStoryId || !currentCanvasElement) {
+		return;
+	}
+	const resultsPath = await server.commands.getResultsPath();
+	await page.screenshot({ path: `${resultsPath}/${currentStoryId}.png` });
+	await server.commands.writeFile(
+		`${resultsPath}/${currentStoryId}.html`,
+		currentCanvasElement.innerHTML,
+	);
+});
