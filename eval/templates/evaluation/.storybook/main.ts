4a07526876e5bbeeceebeeee2077c2da240cfe82
Valentin Palkovic <valentin@chromatic.com>
2026-01-07 20:22:52 +0100

    Update Storybook configuration to correctly include stories from both 'stories' and 'src' directories.

--- eval/templates/evaluation/.storybook/main.ts
+++ eval/templates/evaluation/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
 	stories: [
-		'../(stories|src)/*.stories.@(js|jsx|mjs|ts|tsx)',
+		'../@(stories|src)/*.stories.@(js|jsx|mjs|ts|tsx)',
 		'../results/*.mdx',
 	],
 	addons: [
